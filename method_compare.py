30a4c12a4cb2811a43c76ba47f02c932c214226c
mpesesky <mwpesesky@gmail.com>
2018-06-29 15:41:49 -0700

    Scripts to compare existing and parametric results.

---
+++ method_compare.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description="Match results from parametric and sequence comparison tests")
+
+parser.add_argument("Para", help="Parametic outlier genes")
+parser.add_argument("Mosaic", help="Seq-compare mosaic genes")
+parser.add_argument("Outtable", help="Desired name of output file")
+
+args = parser.parse_args()
+
+para = pd.read_table(args.Para, sep="\t", index_col=0)
+mosaic = pd.read_table(args.Mosaic, sep="\t", index_col=0)
+
+combo = para.merge(mosaic, how='left', left_on='protein_id', right_on='geneID')
+combo.fillna("None")
+combo.to_csv(args.Outtable, sep="\t")
